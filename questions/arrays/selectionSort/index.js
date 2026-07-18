70bcdec88d911fe04968683fde0bb0e93df0e688
Michelle Anderson <manderson0102@yahoo.com>
2026-07-18 16:34:16 -0400

    changed if statement

--- questions/arrays/selectionSort/index.js
+++ questions/arrays/selectionSort/index.js
@@ -12,13 +12,13 @@
 const selectionSort = (nums) => {
     for (let i = 0; i < nums.length; i++){
         let currMin = i
-        
+
         for (let j = i + 1; j < nums.length; j++){
             if (nums[j] < nums[currMin]){
                 currMin = j
             }
         }
-       if (
+       if (currMin !== i){
         let temp = nums[i]
         nums[i] = nums[currMin]
         nums[currMin] = temp
